4955b87bdfa787dae8e788382d3fbfdf98f24d59
r4b2 <github@r4b2.de>
2025-01-28 22:58:49 +0100

    wip

--- main.py
+++ main.py
@@ -11,29 +11,57 @@ sys.path.append(os.path.join(parent_folder_path, "plugin"))
 from flowlauncher import FlowLauncher
 import webbrowser
 
+import requests
+
 
 class HelloWorld(FlowLauncher):
     def query(self, query):
-        return [
-            {
-                "Title": "Hello World, this is where title goes. {}".format(
-                    ("Your query is: " + query, query)[query == ""]
-                ),
-                "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
-                "IcoPath": "Images/app.png",
-                "JsonRPCAction": {
-                    "method": "open_url",
-                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"],
-                },
-            }
-        ]
+        categories = {}
+        response = requests.get("http://127.0.0.1:5000/soundpad/query/" + query)
+
+        # categories look like the following
+        # {
+        #     "23": "57324-concerning-hobbits-lord-of-the-rings",
+        #     "22": "47745-top-12-hobbit-lotr-horns"
+        # }
+
+        if query == "":
+            return [
+                {
+                    "Title": "Search for a sound",
+                    "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
+                    "IcoPath": "Images/soundpad.png",
+                    "JsonRPCAction": {
+                        "method": "open_url",
+                        "parameters": [
+                            "https://github.com/Flow-Launcher/Flow.Launcher"
+                        ],
+                    },
+                }
+            ]
+
+        categories = response.json()
+        results = []
+        for cat_id, cat_name in categories.items():
+            results.append(
+                {
+                    "Title": cat_name,
+                    "SubTitle": "SoundId: " + cat_id,
+                    "IcoPath": "Images/soundpad.png",
+                    "JsonRPCAction": {
+                        "method": "query_set",
+                        "parameters": [cat_id],
+                    },
+                }
+            )
+        return results
 
     def context_menu(self, data):
         return [
             {
                 "Title": "Hello World Python's Context menu",
                 "SubTitle": "Press enter to open Flow the plugin's repo in GitHub",
-                "IcoPath": "Images/app.png",
+                "IcoPath": "Images/soundpad.png",
                 "JsonRPCAction": {
                     "method": "open_url",
                     "parameters": [
@@ -45,6 +73,9 @@ class HelloWorld(FlowLauncher):
 
     def open_url(self, url):
         webbrowser.open(url)
+
+    def query_set(self, cat_id):
+        requests.get("http://127.0.0.1:5000/soundpad/query-set/" + cat_id)
 
 
 if __name__ == "__main__":
